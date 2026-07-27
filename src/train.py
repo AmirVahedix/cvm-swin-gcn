f1c0a207e414ca7796fff61a1bff0be5cf7d8281
Amir Vahedi <amirvahedix@gmail.com>
2026-07-27 21:59:27 +0330

    fixed minio integration

--- src/train.py
+++ src/train.py
@@ -30,8 +30,8 @@ load_dotenv()
 s3_client = boto3.client(
     "s3",
     endpoint_url=os.getenv("MINIO_ENDPOINT"),
-    aws_access_key_id=os.getenv("MINIO_ACCESS_KEY"),
-    aws_secret_access_key=os.getenv("MINIO_SECRET_KEY"),
+    aws_access_key_id=os.getenv("MINIO_USERNAME") or os.getenv("MINIO_USER") or os.getenv("MINIO_ACCESS_KEY"),
+    aws_secret_access_key=os.getenv("MINIO_PASSWORD") or os.getenv("MINIO_SECRET_KEY"),
 )
 
 BUCKET_NAME = os.getenv("MINIO_BUCKET", "cvm-artifacts")
